970ec57341d04cf5a009717444611ddf8296f60f
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-11-22 16:27:10 +0300

    user registration template added

--- project/project/urls.py
+++ project/project/urls.py
@@ -20,8 +20,10 @@ from django.conf.urls.static import static
 from . import views
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls') ),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
