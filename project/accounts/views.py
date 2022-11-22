6dc42acfae8259d156e6ec0b31fb24a9b7f7513a
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-11-23 00:17:46 +0300

    last edits done for user model

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from .models import User
+from django.contrib import messages
 # Create your views here.
  
 def registerUser(request):
-    return render(request, 'register.html')
+
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # password = form.cleaned_data['password']
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # user.save()
+            # return redirect('registerUser')
+
+            # ustteki kisim asaginin diger yolu
+
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.CUSTOMER
+            user.save()
+            messages.success(request, 'Your account has been registered successfully!')
+            return redirect('registerUser')
+        
+        else:
+            print('Invalid Form')
+            print(form.errors)
+            
+    else:
+        form = UserForm()
+
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'registerUser.html', context)
